6f2504b1f240e167340706f15a272d04a9dcce73
Sharan <sharanalwar57@gmail.com>
2025-07-04 20:30:39 +0530

    commit

--- dev/s3.tf
+++ dev/s3.tf
@@ -1,26 +1,26 @@
-# resource "aws_s3_bucket" "my_bucket" {
-#   bucket = "my-unique-bucket-name-123456"  # 🔁 Must be globally unique
-#   force_destroy = true                     # 💣 Allows full delete, including non-empty bucket
-#   tags = {
-#     Name        = "MyBucket"
-#     Environment = "dev"
-#   }
-# }
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-123456"  # 🔁 Must be globally unique
+  force_destroy = true                     # 💣 Allows full delete, including non-empty bucket
+  tags = {
+    Name        = "MyBucket"
+    Environment = "dev"
+  }
+}
 
-# resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
-#   bucket = aws_s3_bucket.my_bucket.id
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+  bucket = aws_s3_bucket.my_bucket.id
 
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# resource "aws_s3_bucket_public_access_block" "my_bucket_block" {
-#   bucket = aws_s3_bucket.my_bucket.id
+resource "aws_s3_bucket_public_access_block" "my_bucket_block" {
+  bucket = aws_s3_bucket.my_bucket.id
 
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
