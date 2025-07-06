5023131be7ddaee146bfc5aa0a2ca0eed9b74477
Sharan-a57 <sharanalwar57@gmail.com>
2025-07-06 13:19:29 +0530

    destroying

--- dev/ec2.tf
+++ dev/ec2.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "github_runner" {
-  ami                    = "ami-020cba7c55df1f615"
-  instance_type          = "t2.micro"
-  key_name               = "orca"
-  subnet_id              = "subnet-0e6e74cce606d5543"
-  vpc_security_group_ids = ["sg-03ceef03176846b70"]
-
-  tags = {
-    Name = "github_runner"
-  }
-}
+#resource "aws_instance" "github_runner" {
+#  ami                    = "ami-020cba7c55df1f615"
+#  instance_type          = "t2.micro"
+#  key_name               = "orca"
+#  subnet_id              = "subnet-0e6e74cce606d5543"
+#  vpc_security_group_ids = ["sg-03ceef03176846b70"]
+#
+#  tags = {
+#    Name = "github_runner"
+#  }
+#}
--- dev/main.tf
+++ dev/main.tf
@@ -1,83 +1,83 @@
-# Configure the AWS Provider
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+# # Configure the AWS Provider
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = var.aws_region
-}
+# # Configure the AWS Provider
+# provider "aws" {
+#   region = var.aws_region
+# }
 
-# Create a security group
-resource "aws_security_group" "web_sg" {
-  name_prefix = "${var.instance_name}-sg-"
-  description = "Security group for ${var.instance_name}"
+# # Create a security group
+# resource "aws_security_group" "web_sg" {
+#   name_prefix = "${var.instance_name}-sg-"
+#   description = "Security group for ${var.instance_name}"
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
-  }
+#   ingress {
+#     description = "SSH"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = var.allowed_cidr_blocks
+#   }
 
- ingress {
-    description = "All ports"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
-  }
+#  ingress {
+#     description = "All ports"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "tcp"
+#     cidr_blocks = var.allowed_cidr_blocks
+#   }
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
-  }
+#   ingress {
+#     description = "HTTP"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = var.allowed_cidr_blocks
+#   }
 
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
-  }
+#   ingress {
+#     description = "HTTPS"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = var.allowed_cidr_blocks
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "${var.instance_name}-security-group"
-  }
-}
+#   tags = {
+#     Name = "${var.instance_name}-security-group"
+#   }
+# }
 
-# Create EC2 instance
-resource "aws_instance" "web_server" {
-  ami           = var.ubuntu
-  instance_type = var.instance_type
-  key_name      = var.key_pair_name
+# # Create EC2 instance
+# resource "aws_instance" "web_server" {
+#   ami           = var.ubuntu
+#   instance_type = var.instance_type
+#   key_name      = var.key_pair_name
   
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+#   vpc_security_group_ids = [aws_security_group.web_sg.id]
   
-  root_block_device {
-    volume_type = var.volume_type
-    volume_size = var.volume_size
-    encrypted   = true
-  }
+#   root_block_device {
+#     volume_type = var.volume_type
+#     volume_size = var.volume_size
+#     encrypted   = true
+#   }
 
-  tags = {
-    Name        = var.instance_name
-    Environment = var.environment
-  }
-}
+#   tags = {
+#     Name        = var.instance_name
+#     Environment = var.environment
+#   }
+# }
